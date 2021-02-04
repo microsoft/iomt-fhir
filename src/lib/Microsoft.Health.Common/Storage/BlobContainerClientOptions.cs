396646a8ac7287c45cb04f4f2ded6f8427c7acc8
Will Yochum <wiyochum@microsoft.com>
2021-02-03 16:52:26 -0800

    add storage files

---
+++ src/lib/Microsoft.Health.Common/Storage/BlobContainerClientOptions.cs
@@ -0,0 +1,20 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using System;
+
+namespace Microsoft.Health.Common.Storage
+{
+    public class BlobContainerClientOptions
+    {
+        public Uri BlobStorageContainerUri { get; set; }
+
+        public string ConnectionString { get; set; }
+
+        public bool ServiceManagedIdentityAuth { get; set; }
+
+        public bool CustomizedAuth { get; set; }
+    }
+}
