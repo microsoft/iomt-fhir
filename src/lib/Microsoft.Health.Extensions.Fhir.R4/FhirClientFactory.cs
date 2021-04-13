661868681bf1a6654bd492a61b94732997d200b6
Will <59618266+wi-y@users.noreply.github.com>
2021-04-13 09:18:00 -0700

    Console app - use managed identity when connecting to fhir server (#94)
    * console app: support mi when connecting to fhir server

--- src/lib/Microsoft.Health.Extensions.Fhir.R4/FhirClientFactory.cs
+++ src/lib/Microsoft.Health.Extensions.Fhir.R4/FhirClientFactory.cs
@@ -3,14 +3,17 @@
 // Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
 // -------------------------------------------------------------------------------------------------
 
+using System;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading;
 using System.Threading.Tasks;
+using Azure.Core;
 using EnsureThat;
 using Hl7.Fhir.Rest;
 using Microsoft.Extensions.Options;
 using Microsoft.Health.Common;
+using Microsoft.Health.Common.Auth;
 using Microsoft.Health.Extensions.Fhir.Config;
 using Microsoft.Health.Extensions.Host.Auth;
 
@@ -19,6 +22,7 @@ namespace Microsoft.Health.Extensions.Fhir
     public class FhirClientFactory : IFactory<FhirClient>
     {
         private readonly bool _useManagedIdentity = false;
+        private readonly IAzureCredentialProvider _tokenCredentialProvider;
 
         public FhirClientFactory(IOptions<FhirClientFactoryOptions> options)
             : this(EnsureArg.IsNotNull(options, nameof(options)).Value.UseManagedIdentity)
@@ -35,26 +39,37 @@ namespace Microsoft.Health.Extensions.Fhir
             _useManagedIdentity = useManagedIdentity;
         }
 
+        public FhirClientFactory(IAzureCredentialProvider provider)
+        {
+            _tokenCredentialProvider = provider;
+        }
+
         public static IFactory<FhirClient> Instance { get; } = new FhirClientFactory();
 
         public FhirClient Create()
         {
+            if (_tokenCredentialProvider != null)
+            {
+                return CreateClient(_tokenCredentialProvider.GetCredential());
+            }
+
             return _useManagedIdentity ? CreateManagedIdentityClient() : CreateConfidentialApplicationClient();
         }
 
-        private static FhirClient CreateClient(IAuthService authService)
+        private static FhirClient CreateClient(TokenCredential tokenCredential)
         {
-            var url = System.Environment.GetEnvironmentVariable("FhirService:Url");
+            var url = Environment.GetEnvironmentVariable("FhirService:Url");
             EnsureArg.IsNotNullOrEmpty(url, nameof(url));
+            var uri = new Uri(url);
 
-            EnsureArg.IsNotNull(authService, nameof(authService));
+            EnsureArg.IsNotNull(tokenCredential, nameof(tokenCredential));
 
             var fhirClientSettings = new FhirClientSettings
             {
                 PreferredFormat = ResourceFormat.Json,
             };
 
-            var client = new FhirClient(url, fhirClientSettings, new BearerTokenAuthorizationMessageHandler(authService));
+            var client = new FhirClient(url, fhirClientSettings, new BearerTokenAuthorizationMessageHandler(uri, tokenCredential));
 
             return client;
         }
@@ -71,19 +86,26 @@ namespace Microsoft.Health.Extensions.Fhir
 
         private class BearerTokenAuthorizationMessageHandler : HttpClientHandler
         {
-            public BearerTokenAuthorizationMessageHandler(IAuthService authService)
+            public BearerTokenAuthorizationMessageHandler(Uri uri, TokenCredential tokenCredentialProvider)
             {
-                AuthService = EnsureArg.IsNotNull(authService, nameof(authService));
+                TokenCredential = EnsureArg.IsNotNull(tokenCredentialProvider, nameof(tokenCredentialProvider));
+                Uri = EnsureArg.IsNotNull(uri);
+                Scopes = new string[] { Uri + ".default" };
             }
 
-            private IAuthService AuthService { get; }
+            private TokenCredential TokenCredential { get; }
+
+            private Uri Uri { get; }
+
+            private string[] Scopes { get; }
 
             protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
             {
-                var token = await AuthService.GetAccessTokenAsync();
-                request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
-
-                return await base.SendAsync(request, cancellationToken);
+                var requestContext = new TokenRequestContext(Scopes);
+                var accessToken = await TokenCredential.GetTokenAsync(requestContext, CancellationToken.None);
+                request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", accessToken.Token);
+                var response = await base.SendAsync(request, cancellationToken);
+                return response;
             }
         }
     }
