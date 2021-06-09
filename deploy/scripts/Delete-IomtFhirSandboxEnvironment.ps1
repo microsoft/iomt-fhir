98594bc86e5950b585172d6dfb5f806290caaa4f
Pallavi Reddy <84752343+pallar-ms@users.noreply.github.com>
2021-06-09 10:36:00 -0700

    cleanup delete scripts

--- deploy/scripts/Delete-IomtFhirSandboxEnvironment.ps1
+++ deploy/scripts/Delete-IomtFhirSandboxEnvironment.ps1
@@ -12,14 +12,6 @@ param
 
 Set-StrictMode -Version Latest
 
-# Get current AzureAd context
-try {
-    $tenantInfo = Get-AzureADCurrentSessionInfo -ErrorAction Stop
-} 
-catch {
-    throw "Please log in to Azure AD with Connect-AzureAD cmdlet before proceeding"
-}
-
 # Get current Az context
 try {
     $azContext = Get-AzContext
@@ -27,9 +19,6 @@ try {
 catch {
     throw "Please log in to Azure RM with Login-AzAccount cmdlet before proceeding"
 }
-
-# Set up Auth Configuration and Resource Group
-./Delete-IomtFhirSandboxAuthConfig.ps1 -EnvironmentName $EnvironmentName 
 
 # Wipe out the environment
 Get-AzResourceGroup -Name $EnvironmentName | Remove-AzResourceGroup -Verbose -Force
