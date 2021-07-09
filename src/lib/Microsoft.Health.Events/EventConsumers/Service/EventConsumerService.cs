6ae29db9d86f797000b3a6a7d56e1ef200754a88
Rob Gordon <rogordon@microsoft.com>
2021-07-09 15:23:19 -0700

    Use Polly to perform retries on failed event processing

--- src/lib/Microsoft.Health.Events/EventConsumers/Service/EventConsumerService.cs
+++ src/lib/Microsoft.Health.Events/EventConsumers/Service/EventConsumerService.cs
@@ -6,23 +6,38 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
+using Azure;
+using Azure.Messaging.EventHubs;
+using EnsureThat;
 using Microsoft.Health.Events.Model;
 using Microsoft.Health.Logging.Telemetry;
+using Polly;
 
 namespace Microsoft.Health.Events.EventConsumers.Service
 {
     public class EventConsumerService : IEventConsumerService
     {
         private readonly IEnumerable<IEventConsumer> _eventConsumers;
-        private const int _maximumBackoffMs = 32000;
         private ITelemetryLogger _logger;
 
         public EventConsumerService(IEnumerable<IEventConsumer> eventConsumers, ITelemetryLogger logger)
         {
-            _eventConsumers = eventConsumers;
-            _logger = logger;
+            _eventConsumers = EnsureArg.IsNotNull(eventConsumers, nameof(eventConsumers));
+            _logger = EnsureArg.IsNotNull(logger, nameof(logger));
+
+            RetryPolicy = CreateRetryPolicy(logger);
         }
+
+        public EventConsumerService(IEnumerable<IEventConsumer> eventConsumers, ITelemetryLogger logger, AsyncPolicy retryPolicy)
+        {
+            _eventConsumers = EnsureArg.IsNotNull(eventConsumers, nameof(eventConsumers));
+            _logger = EnsureArg.IsNotNull(logger, nameof(logger));
+            RetryPolicy = EnsureArg.IsNotNull(retryPolicy, nameof(retryPolicy));
+        }
+
+        public AsyncPolicy RetryPolicy { get;  }
 
         public Task ConsumeEvent(IEventMessage eventArg)
         {
@@ -35,37 +50,14 @@ namespace Microsoft.Health.Events.EventConsumers.Service
             {
                 foreach (IEventConsumer eventConsumer in _eventConsumers)
                 {
-                    await OperationWithRetryAsync(eventConsumer, events);
-                }
-            }
-        }
-
-        private async Task OperationWithRetryAsync(IEventConsumer eventConsumer, IEnumerable<IEventMessage> events)
-        {
-            int currentRetry = 0;
-            double backoffMs = 0;
-            Random random = new Random();
-            bool operationComplete = false;
-
-            while (!operationComplete)
-            {
-                try
-                {
-                    if (currentRetry > 0 && backoffMs < _maximumBackoffMs)
+                    try
                     {
-                        int randomMs = random.Next(0, 1000);
-                        backoffMs = Math.Pow(2000, currentRetry) + randomMs;
-                        await Task.Delay((int)backoffMs);
+                        await RetryPolicy.ExecuteAsync(async () => await TryOperationAsync(eventConsumer, events).ConfigureAwait(false));
                     }
-
-                    await TryOperationAsync(eventConsumer, events).ConfigureAwait(false);
-                    break;
-                }
-#pragma warning disable CA1031
-                catch (Exception e)
-#pragma warning restore CA1031
-                {
-                    _logger.LogError(e);
+                    catch (Exception e)
+                    {
+                        _logger.LogError(e);
+                    }
                 }
             }
         }
@@ -73,6 +65,31 @@ namespace Microsoft.Health.Events.EventConsumers.Service
         private static async Task TryOperationAsync(IEventConsumer eventConsumer, IEnumerable<IEventMessage> events)
         {
             await eventConsumer.ConsumeAsync(events);
+        }
+
+        private static AsyncPolicy CreateRetryPolicy(ITelemetryLogger logger)
+        {
+            bool ExceptionRetryableFilter(Exception ee)
+            {
+                switch (ee)
+                {
+                    case AggregateException ae when ae.InnerExceptions.All(ExceptionRetryableFilter):
+                    case OperationCanceledException _:
+                    case HttpRequestException _:
+                    case EventHubsException _:
+                    case RequestFailedException _:
+                        break;
+                    default:
+                        return false;
+                }
+
+                logger.LogError(new Exception("Encountered retryable exception", ee));
+                return true;
+            }
+
+            return Policy
+                .Handle<Exception>(ExceptionRetryableFilter)
+                .WaitAndRetryForeverAsync(retryCount => TimeSpan.FromSeconds(Math.Min(30, Math.Pow(2, retryCount))));
         }
     }
 }
--- test/Microsoft.Health.Events.UnitTest/EventConsumerServiceTests.cs
+++ test/Microsoft.Health.Events.UnitTest/EventConsumerServiceTests.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.Health.Events.EventConsumers;
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using Microsoft.Health.Events.EventConsumers;
 using Microsoft.Health.Events.EventConsumers.Service;
 using Microsoft.Health.Events.Model;
 using Microsoft.Health.Logging.Telemetry;
@@ -6,6 +11,7 @@ using NSubstitute;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Net.Http;
 using Xunit;
 
 namespace Microsoft.Health.Events.UnitTest
@@ -13,7 +19,30 @@ namespace Microsoft.Health.Events.UnitTest
     public class EventConsumerServiceTests
     {
         [Fact]
-        public async void GivenEventConsumer_WhenEventConsumerThrowsException_ThenRetryEventConsumer_Test()
+        public async void GivenEventConsumer_WhenEventConsumerThrowsNonRetryableException_ThenEventConsumer_IsNotRetried_()
+        {
+            var retries = 0;
+            var initialBatch = new List<EventMessage>()
+            {
+                new EventMessage("0", new ReadOnlyMemory<byte>(), 1, 1, new DateTime(2020, 12, 31, 5, 10, 20), new Dictionary<string, object>(), new ReadOnlyDictionary<string, object>(new Dictionary<string, object>()))
+            };
+
+            var logger = Substitute.For<ITelemetryLogger>();
+            var eventConsumer = Substitute.For<IEventConsumer>();
+
+            eventConsumer.When(x => x.ConsumeAsync(initialBatch))
+                .Do(x => { if (retries < 3) { retries++;  throw new Exception("failure"); } });
+
+            var eventEventConsumers = new List<IEventConsumer>() { eventConsumer };
+            var eventConsumerService = new EventConsumerService(eventEventConsumers, logger);
+
+            await eventConsumerService.ConsumeEvents(initialBatch);
+            logger.Received(1).LogError(Arg.Is<Exception>(ex => ex.Message == "failure"));
+            await eventConsumer.Received(1).ConsumeAsync(initialBatch);
+        }
+
+        [Fact]
+        public async void GivenEventConsumer_WhenEventConsumerThrowsRetryableException_ThenRetryEventConsumer_Test()
         {
             var retries = 0;
             var initialBatch = new List<EventMessage>()
@@ -26,14 +55,15 @@ namespace Microsoft.Health.Events.UnitTest
 
             // fail to consume events 3 times in a row, then succeed
             eventConsumer.When(x => x.ConsumeAsync(initialBatch))
-                .Do(x => { if (retries < 3) { retries++;  throw new Exception("failure"); } });
+                .Do(x => { if (retries < 2) { retries++; throw new HttpRequestException("failure"); } });
 
             var eventEventConsumers = new List<IEventConsumer>() { eventConsumer };
             var eventConsumerService = new EventConsumerService(eventEventConsumers, logger);
 
-            // 3 retries followed by a successful call
+            // 2 retries followed by a successful call
             await eventConsumerService.ConsumeEvents(initialBatch);
-            await eventConsumer.Received(4).ConsumeAsync(initialBatch);
+            logger.Received(2).LogError(Arg.Is<Exception>(ex => ex.Message.StartsWith("Encountered retryable exception")));
+            await eventConsumer.Received(3).ConsumeAsync(initialBatch);
         }
     }
 }
