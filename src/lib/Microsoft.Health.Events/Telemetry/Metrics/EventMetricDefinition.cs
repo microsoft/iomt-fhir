511265645126b1e95963869af0056000091a562f
William Yochum <wiyochum@microsoft.com>
2021-10-08 10:40:12 -0400

    add event metric definition

---
+++ src/lib/Microsoft.Health.Events/Telemetry/Metrics/EventMetricDefinition.cs
@@ -0,0 +1,32 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+namespace Microsoft.Health.Events.Telemetry
+{
+    public sealed class EventMetricDefinition
+    {
+        private EventMetricDefinition(string metricName)
+        {
+            MetricName = metricName;
+        }
+
+        public string MetricName { get; }
+
+        public static EventMetricDefinition DeviceIngressSizeBytes()
+        {
+            return new EventMetricDefinition(nameof(DeviceIngressSizeBytes));
+        }
+
+        public static EventMetricDefinition MeasurementToFhirBytes()
+        {
+            return new EventMetricDefinition(nameof(MeasurementToFhirBytes));
+        }
+
+        public override string ToString()
+        {
+            return MetricName;
+        }
+    }
+}
