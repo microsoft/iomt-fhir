22cdbf3768e0d11df802a1e04f12f832ab3c28c9
Carrie Lai <clai@microsoft.com>
2021-10-12 08:06:07 -0700

    Separated constants from interfaces, as per review comment.

--- tools/data-mapper/Microsoft.Health.Tools.DataMapper/ClientApp/src/store/Mapping.Constants.ts
+++ tools/data-mapper/Microsoft.Health.Tools.DataMapper/ClientApp/src/store/Mapping.Constants.ts
@@ -3,7 +3,9 @@
 // Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
 // -------------------------------------------------------------------------------------------------
 
-export * from './Mapping.Actions';
-export * from './Mapping.Constants';
-export * from './Mapping.Reducers';
-export * from './Mapping.States';
+export const FhirValueType = {
+    CodeableConcept: "CodeableConcept",
+    Quantity: "Quantity",
+    SampledData: "SampledData",
+    String: "String"
+}
