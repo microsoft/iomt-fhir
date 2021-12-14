fe567bbcce8f4375835200fac5f7d5118465d502
Carrie Lai <clai@microsoft.com>
2021-12-14 11:31:31 -0800

    Localized exception messages.

--- src/lib/Microsoft.Health.Extensions.Fhir.R4/Telemetry/Exceptions/FhirServiceExceptionProcessor.cs
+++ src/lib/Microsoft.Health.Extensions.Fhir.R4/Telemetry/Exceptions/FhirServiceExceptionProcessor.cs
@@ -9,6 +9,7 @@ using System.Net.Http;
 using EnsureThat;
 using Hl7.Fhir.Rest;
 using Microsoft.Health.Common.Telemetry;
+using Microsoft.Health.Extensions.Fhir.Resources;
 using Microsoft.Health.Extensions.Fhir.Telemetry.Metrics;
 using Microsoft.Health.Logging.Telemetry;
 using Microsoft.Identity.Client;
@@ -45,12 +46,12 @@ namespace Microsoft.Health.Extensions.Fhir.Telemetry.Exceptions
                     switch (status)
                     {
                         case HttpStatusCode.Forbidden:
-                            message = "Verify that the provided FHIR service's 'FHIR Data Writer' role has been assigned to the applicable Azure Active Directory security principal or managed identity.";
+                            message = FhirResources.FhirServiceAccessForbidden;
                             string helpLink = "https://docs.microsoft.com/azure/healthcare-apis/iot/deploy-iot-connector-in-azure#accessing-the-iot-connector-from-the-fhir-service";
                             errorName = nameof(FhirServiceErrorCode.AuthorizationError);
                             return (new UnauthorizedAccessFhirServiceException(message, exception, helpLink, errorName), errorName);
                         case HttpStatusCode.NotFound:
-                            message = "Verify that the provided URL is for a FHIR service.";
+                            message = FhirResources.FhirServiceNotFound;
                             errorName = nameof(FhirServiceErrorCode.ConfigurationError);
                             return (new InvalidFhirServiceException(message, exception, errorName), errorName);
                         default:
@@ -58,18 +59,18 @@ namespace Microsoft.Health.Extensions.Fhir.Telemetry.Exceptions
                     }
 
                 case UriFormatException _:
-                    message = "Verify that the provided FHIR service URL is an absolute URL.";
+                    message = FhirResources.FhirServiceUriFormatInvalid;
                     errorName = nameof(FhirServiceErrorCode.ConfigurationError);
                     return (new InvalidFhirServiceException(message, exception, errorName), errorName);
 
                 case HttpRequestException _:
-                    message = "Verify that the provided FHIR service URL exists.";
+                    message = FhirResources.FhirServiceHttpRequestError;
                     errorName = nameof(FhirServiceErrorCode.ConfigurationError);
                     return (new InvalidFhirServiceException(message, exception, errorName), errorName);
 
                 case MsalServiceException _:
                     string errorCode = ((MsalServiceException)exception).ErrorCode;
-                    message = "Verify that the provided FHIR service URL is a base URL and exists in the Microsoft tenant.";
+                    message = FhirResources.FhirServiceMsalServiceError;
                     errorName = $"{nameof(FhirServiceErrorCode.ConfigurationError)}_{errorCode}";
                     return (new InvalidFhirServiceException(message, exception, errorName), errorName);
 
