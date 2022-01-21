551e55ff4943ed0248b44cbbe08eb568a15e435d
Carrie Lai <clai@microsoft.com>
2022-01-21 10:10:03 -0800

    Nit change to error message when FHIR service doesn't exist.

--- src/lib/Microsoft.Health.Extensions.Fhir/Resources/FhirResources.Designer.cs
+++ src/lib/Microsoft.Health.Extensions.Fhir/Resources/FhirResources.Designer.cs
@@ -19,7 +19,7 @@ namespace Microsoft.Health.Extensions.Fhir.Resources {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class FhirResources {
@@ -79,7 +79,7 @@ namespace Microsoft.Health.Extensions.Fhir.Resources {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Verify that the provided FHIR service URL exists..
+        ///   Looks up a localized string similar to Verify that the provided FHIR service exists..
         /// </summary>
         public static string FhirServiceHttpRequestError {
             get {
