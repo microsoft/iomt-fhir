792486ba9985dffb9c648a09e90f9da5a9d40a69
Rob Gordon <rogordon@microsoft.com>
2022-05-09 10:22:25 -0700

    Removing uneeded code

--- src/lib/Microsoft.Health.Fhir.Ingest.Template/TemplateContainer.cs
+++ src/lib/Microsoft.Health.Fhir.Ingest.Template/TemplateContainer.cs
@@ -10,26 +10,17 @@ using Newtonsoft.Json.Linq;
 namespace Microsoft.Health.Fhir.Ingest.Template
 {
     [JsonConverter(typeof(TemplateContainerJsonConverter))]
-    public class TemplateContainer : IJsonLineInfo
+    public class TemplateContainer
     {
         public string TemplateType { get; set; }
 
         public JToken Template { get; set; }
-
-        public int LineNumber { get; set; }
-
-        public int LinePosition { get; set; }
 
         public bool MatchTemplateName(string expected)
         {
             const string TemplateSuffix = "Template";
             return StringComparer.InvariantCultureIgnoreCase.Equals(expected, TemplateType)
                 || StringComparer.InvariantCultureIgnoreCase.Equals(expected, TemplateType + TemplateSuffix);
-        }
-
-        bool IJsonLineInfo.HasLineInfo()
-        {
-            return true;
         }
     }
 }
