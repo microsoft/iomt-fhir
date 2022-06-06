8864425200bd959935e01b0cd58cfcce14ccd6c7
Rob Gordon <rogordon@microsoft.com>
2022-06-06 10:01:53 -0400

    Create JToken directly from event data versus first creating a String

--- src/lib/Microsoft.Health.Fhir.Ingest/Data/EventDataWithJsonBodyToJTokenConverter.cs
+++ src/lib/Microsoft.Health.Fhir.Ingest/Data/EventDataWithJsonBodyToJTokenConverter.cs
@@ -3,9 +3,11 @@
 // Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
 // -------------------------------------------------------------------------------------------------
 
+using System.IO;
 using System.Text;
 using EnsureThat;
 using Microsoft.Azure.EventHubs;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
 namespace Microsoft.Health.Fhir.Ingest.Data
@@ -15,10 +17,18 @@ namespace Microsoft.Health.Fhir.Ingest.Data
         public JToken Convert(EventData input)
         {
             EnsureArg.IsNotNull(input, nameof(input));
+            var body = null as JToken;
 
-            var body = input.Body.Count > 0
-                ? JToken.Parse(Encoding.UTF8.GetString(input.Body.Array, input.Body.Offset, input.Body.Count))
-                : null;
+            if (input.Body.Count > 0)
+            {
+                using (var stream = new MemoryStream(input.Body.Array))
+                using (StreamReader sr = new StreamReader(stream, Encoding.UTF8))
+                using (JsonReader reader = new JsonTextReader(sr))
+                {
+                    body = JToken.ReadFrom(reader);
+                }
+            }
+
             var data = new { Body = body, input.Properties, input.SystemProperties };
             var token = JToken.FromObject(data);
             return token;
