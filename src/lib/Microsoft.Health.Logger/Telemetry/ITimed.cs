27cefdb27b36b3d0ed89dfc21a91410533a906fd
Dustin Burson <47367432+dustinburson@users.noreply.github.com>
2022-07-18 10:31:25 -0700

    Add ability to easily track call duration and emit to a metric (#217)
    * Add ITimed and extensions to easily track durations and emit metrics.

---
+++ src/lib/Microsoft.Health.Logger/Telemetry/ITimed.cs
@@ -0,0 +1,21 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using Microsoft.Health.Common.Telemetry;
+using System;
+
+namespace Microsoft.Health.Logging.Telemetry
+{
+    public interface ITimed : IDisposable
+    {
+        ITelemetryLogger Log { get; }
+
+        TimeSpan Elapsed { get; }
+
+        ITimed Record();
+
+        ITimed Record(Metric metric);
+    }
+}
---
+++ src/lib/Microsoft.Health.Logger/Telemetry/TimedExtensions.cs
@@ -0,0 +1,64 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using EnsureThat;
+using Microsoft.Health.Common.Telemetry;
+using System;
+using System.Diagnostics;
+
+namespace Microsoft.Health.Logging.Telemetry
+{
+    public static class TimedExtensions
+    {
+
+        /// <summary>
+        /// Creates an ITimed object around the provided <see cref="ITelemetryLogger"/> and tracks the duration till the ITimed object is disposed and emits the duration to the <see cref="ITelemetryLogger"/>
+        /// </summary>
+        /// <param name="logger">The <see cref="ITelemetryLogger"/> the duration is emitted to when the ITimed object is disposed</param>
+        /// <param name="metric">The metric to record the tracked duration to.</param>
+        /// <returns>ITimed object wrapping the provided log</returns>
+        public static ITimed TrackDuration(this ITelemetryLogger logger, Metric metric)
+        {
+            EnsureArg.IsNotNull(logger, nameof(logger));
+            EnsureArg.IsNotNull(metric, nameof(metric));
+
+            return new TimedLog
+            {
+                Stopwatch = Stopwatch.StartNew(),
+                Log = logger,
+                Metric = metric,
+            };
+        }
+
+        private struct TimedLog : ITimed
+        {
+            public Stopwatch Stopwatch { get; set; }
+
+            public Metric Metric { get; set; }
+
+            public ITelemetryLogger Log { get; set; }
+
+            public TimeSpan Elapsed => Stopwatch.Elapsed;
+
+            public ITimed Record()
+            {
+                return Record(Metric);
+            }
+
+            public ITimed Record(Metric metric)
+            {
+                EnsureArg.IsNotNull(metric, nameof(metric));
+                Log.LogMetric(metric, Elapsed.TotalMilliseconds);
+                return this;
+            }
+
+            public void Dispose()
+            {
+                Stopwatch.Stop();
+                Record();
+            }
+        }
+    }
+}
