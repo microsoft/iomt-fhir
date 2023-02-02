c3f78d1558dd8998e20d58e3e93c601940574f02
Will Yochum <wiyochum@microsoft.com>
2023-02-02 16:10:05 -0500

    catch additional normalization mapping exceptions

--- src/lib/Microsoft.Health.Fhir.Ingest/Service/NormalizationEventConsumerService.cs
+++ src/lib/Microsoft.Health.Fhir.Ingest/Service/NormalizationEventConsumerService.cs
@@ -232,20 +232,20 @@ namespace Microsoft.Health.Fhir.Ingest.Service
 
             using (ITimed normalizeDuration = _logger.TrackDuration(IomtMetrics.NormalizedEventGenerationTimeMs(evt.PartitionId)))
             {
-                foreach (var measurement in template.GetMeasurements(token))
+                try
                 {
-                    try
+                    foreach (var measurement in template.GetMeasurements(token))
                     {
                         measurement.IngestionTimeUtc = evt.EnqueuedTime.UtcDateTime;
                         collector.Add((evt.PartitionId, measurement));
                         projections++;
                     }
-                    catch (Exception ex)
-                    {
-                        // Translate all Normalization Mapping exceptions into a common type for easy identification.
-                        throw new NormalizationDataMappingException(ex, nameof(NormalizationDataMappingException))
-                            .AddEventContext(evt);
-                    }
+                }
+                catch (Exception ex)
+                {
+                    // Translate all Normalization Mapping exceptions into a common type for easy identification.
+                    throw new NormalizationDataMappingException(ex, nameof(NormalizationDataMappingException))
+                        .AddEventContext(evt);
                 }
             }
 
