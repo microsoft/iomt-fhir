24954061fb43ff79f7b02cbd13929e6b1a00c5f4
Will Yochum <wiyochum@microsoft.com>
2023-03-31 14:24:04 -0400

    formatting

--- src/lib/Microsoft.Health.Fhir.R4.Ingest/Service/R4FhirImportService.cs
+++ src/lib/Microsoft.Health.Fhir.R4.Ingest/Service/R4FhirImportService.cs
@@ -131,7 +131,8 @@ namespace Microsoft.Health.Fhir.Ingest.Service
                             // If the version id is not equal to "1" on a create then it is likely two processes modified
                             // the resource at the same time.
                             // However we cannot check for not equal to "1" because a resource can be soft-deleted and recreated
-                            // and on the recreate the resource version will also be not equal to "1", so instead we will check for resource version id = "2"
+                            // and on the recreate the resource version will also be not equal to "1",
+                            // so instead we will check for resource version id = "2"
                             if (result.VersionId == "2")
                             {
                                 _logger.LogMetric(IomtMetrics.FHIRResourceContention(ResourceType.Observation), 1);
