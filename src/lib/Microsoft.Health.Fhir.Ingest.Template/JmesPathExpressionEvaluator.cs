ce06b54ad2dcbc638bd6a1b58df49f6955884f4d
Dustin Burson <47367432+dustinburson@users.noreply.github.com>
2023-05-26 08:37:37 -0700

    Update project packages to latest (#275)
    * Update project packages to latest

--- src/lib/Microsoft.Health.Fhir.Ingest.Template/JmesPathExpressionEvaluator.cs
+++ src/lib/Microsoft.Health.Fhir.Ingest.Template/JmesPathExpressionEvaluator.cs
@@ -6,6 +6,7 @@
 using System;
 using System.Collections.Generic;
 using DevLab.JmesPath;
+using DevLab.JmesPath.Expressions;
 using EnsureThat;
 using Newtonsoft.Json.Linq;
 
@@ -13,7 +14,7 @@ namespace Microsoft.Health.Fhir.Ingest.Template
 {
     public class JmesPathExpressionEvaluator : IExpressionEvaluator
     {
-        private readonly JmesPath.Expression _jmespathExpression;
+        private readonly JmesPathExpression _jmespathExpression;
         private readonly ILineInfo _lineInfo;
 
         public JmesPathExpressionEvaluator(JmesPath jmesPath, string expression, ILineInfo lineInfo)
