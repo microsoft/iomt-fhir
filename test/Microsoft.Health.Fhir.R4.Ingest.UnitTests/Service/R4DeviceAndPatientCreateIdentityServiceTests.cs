e953611a8e78e13c6281c2fafbcbf57b93389959
Dustin Burson <duburson@microsoft.com>
2023-06-20 16:30:14 -0700

    Update the IdentityService to always link and encounter id if specified in the measurement group.
    This will allow optional encounter linking in the LookupIdentityService & CreateIdentityService.
    
    The DeviceAndPatientWithEncounterIdentityService will still require the encounter id present or a ResourceIdentityNotDefinedException will be thrown.

--- test/Microsoft.Health.Fhir.R4.Ingest.UnitTests/Service/R4DeviceAndPatientCreateIdentityServiceTests.cs
+++ test/Microsoft.Health.Fhir.R4.Ingest.UnitTests/Service/R4DeviceAndPatientCreateIdentityServiceTests.cs
@@ -277,5 +277,75 @@ namespace Microsoft.Health.Fhir.Ingest.Service
             await resourceService.Received(1).EnsureResourceByIdentityAsync("deviceId", null, Arg.Any<Action<Model.Device, Model.Identifier>>());
             await resourceService.Received(1).EnsureResourceByIdentityAsync("patientId", null, Arg.Any<Action<Model.Patient, Model.Identifier>>());
         }
+
+        [Fact]
+        public async void GivenValidEncounterIdentifier_WhenResolveResourceIdentitiesAsync_ThenEncounterIdReturned_Test()
+        {
+            var fhirClient = Utilities.CreateMockFhirService();
+            var resourceService = Substitute.For<ResourceManagementService>(fhirClient);
+            var device = new Model.Device
+            {
+                Id = "1",
+                Patient = new Model.ResourceReference("Patient/123"),
+            };
+
+            var encounter = new Model.Encounter
+            {
+                Id = "abc",
+            };
+
+            var mg = Substitute.For<IMeasurementGroup>();
+            mg.DeviceId.Returns("deviceId");
+            mg.EncounterId.Returns("eId");
+
+            resourceService.GetResourceByIdentityAsync<Model.Device>(Arg.Any<string>(), Arg.Any<string>())
+                .Returns(Task.FromResult(device));
+
+            resourceService.GetResourceByIdentityAsync<Model.Encounter>(Arg.Any<string>(), Arg.Any<string>())
+                .Returns(Task.FromResult(encounter));
+
+            using (var idSrv = new R4DeviceAndPatientCreateIdentityService(fhirClient, resourceService))
+            {
+                var ids = await idSrv.ResolveResourceIdentitiesAsync(mg);
+
+                Assert.Equal("1", ids[ResourceType.Device]);
+                Assert.Equal("123", ids[ResourceType.Patient]);
+                Assert.Equal("abc", ids[ResourceType.Encounter]);
+            }
+
+            await resourceService.Received(1).GetResourceByIdentityAsync<Model.Device>("deviceId", null);
+            await resourceService.Received(1).GetResourceByIdentityAsync<Model.Encounter>("eId", null);
+        }
+
+        [Fact]
+        public async void GivenInValidEncounterIdentifier_WhenResolveResourceIdentitiesAsync_ThenFhirResourceNotFoundExceptionThrown_Test()
+        {
+            var fhirClient = Utilities.CreateMockFhirService();
+            var resourceService = Substitute.For<ResourceManagementService>(fhirClient);
+            var device = new Model.Device
+            {
+                Id = "1",
+                Patient = new Model.ResourceReference("Patient/123"),
+            };
+
+            var mg = Substitute.For<IMeasurementGroup>();
+            mg.DeviceId.Returns("deviceId");
+            mg.EncounterId.Returns("eId");
+
+            resourceService.GetResourceByIdentityAsync<Model.Device>(Arg.Any<string>(), Arg.Any<string>())
+                .Returns(Task.FromResult(device));
+
+            resourceService.GetResourceByIdentityAsync<Model.Encounter>(Arg.Any<string>(), Arg.Any<string>())
+                .Returns(Task.FromResult((Model.Encounter)null));
+
+            using (var idSrv = new R4DeviceAndPatientCreateIdentityService(fhirClient, resourceService))
+            {
+                var ex = await Assert.ThrowsAsync<FhirResourceNotFoundException>(async () => await idSrv.ResolveResourceIdentitiesAsync(mg));
+                Assert.Equal(ResourceType.Encounter, ex.FhirResourceType);
+            }
+
+            await resourceService.Received(1).GetResourceByIdentityAsync<Model.Device>("deviceId", null);
+            await resourceService.Received(1).GetResourceByIdentityAsync<Model.Encounter>("eId", null);
+        }
     }
 }
