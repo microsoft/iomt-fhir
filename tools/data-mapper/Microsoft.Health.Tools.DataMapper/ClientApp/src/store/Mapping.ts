72d22d68e2084cc787f0b499296b8d9f3e646a31
Dustin Burson <47367432+dustinburson@users.noreply.github.com>
2023-07-11 09:25:41 -0700

    Set Nuget.config to single feed & remove data mapper (#284)
    * Update NuGet package configuration file in the repository to comply with Microsoft package feed security policies.
    
    * Revert "Update NuGet package configuration file in the repository to comply with Microsoft package feed security policies."
    
    This reverts commit 6a318c5ffd5ad996d8b32a549c2410a0b2c67e4d.
    
    * Remove data-mapper tool.
    
    * Update NuGet package configuration file in the repository to comply with Microsoft package feed security policies.
    
    * Revert "Update NuGet package configuration file in the repository to comply with Microsoft package feed security policies."
    
    This reverts commit 304110719f7e09888a986479cd480f06fde95335.
    
    * Update NuGet package configuration file in the repository to comply with Microsoft package feed security policies.
    
    * Add package lock files to use upstream sources in public feed.
    
    * Remove readme section for data mapper.

--- tools/data-mapper/Microsoft.Health.Tools.DataMapper/ClientApp/src/store/Mapping.ts
+++
@@ -1,9 +0,0 @@
-﻿// -------------------------------------------------------------------------------------------------
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
-// -------------------------------------------------------------------------------------------------
-
-export * from './Mapping.Actions';
-export * from './Mapping.Constants';
-export * from './Mapping.Reducers';
-export * from './Mapping.States';
